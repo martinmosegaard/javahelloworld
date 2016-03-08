8f2ce3a67a4e7bc6e78932632ae23faae6054f30
Martin Mosegaard Amdisen <martin.amdisen@praqma.com>
2016-03-08 15:22:34 +0000

    Awesome edit of the source code

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,7 +1,7 @@
 public class HelloWorld {
 
   public static void main(String[] args) {
-    System.out.println("Hello Java in Docker");
+    System.out.println("Hello Java in Docker, autobuild on Docker Hub triggered from GitHub");
   }
 
 }
